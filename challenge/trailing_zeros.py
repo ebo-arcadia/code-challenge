6bd2440b773a0dd240b65fba7a03b53c6970be95
Ebo Arcadia <yiboli.coding@gmail.com>
2021-11-29 20:54:23 -0500

    add trailing_zeros solution

--- challenge/trailing_zeros.py
+++ challenge/trailing_zeros.py
@@ -21,20 +21,44 @@
 
 
 def trailing_zeros(num):
-    num = int(num)
-    factorial = 1
-    zeros = {}
-    for i in range(1, num+1):
-        # print(i)
-        factorial *= i
-        print(factorial)
-        reversed_factorial = str(factorial)[::-1]
-        print("factory reversed: ", reversed_factorial)
-    for d in map(int, str(factorial)[::-1]):
-        while d == 0:
-            zeros[d] = zeros[d] + 1
-            break
-    return zeros
+    # num = int(num)
+    # factorial = 1
+    # zeros = {}
+    # trailing_zeros_counts = 0
+    # for i in range(1, num+1):
+    #     # print(i)
+    #     factorial *= i
+    #     # reversed_factorial = str(factorial)[::-1]
+    #     # print("factory reversed: ", reversed_factorial)
+    # trailing_zeros_rstrip = len(str(factorial)) - len(str(factorial).rstrip('0'))
+    # print(factorial)
+    # print("get trailing zeros using rstrip: ", trailing_zeros_rstrip)
+    # return trailing_zeros_rstrip
+    #
+    # for d in map(int, str(factorial)[::-1]):
+    #     print(d)
+    #     if d in zeros:
+    #         zeros[d] = zeros[d] + 1
+    #     else:
+    #         zeros[d] = 1
+    # for key in zeros:
+    #     # iterate only if the key is zero
+    #     while key == 0:
+    #         print("zero key value: ", zeros[key])
+    #         trailing_zeros_counts = zeros[key]
+    #         print("total trailing zeros count: ", trailing_zeros_counts)
+    #         break
+    #     else:
+    #         print("all keys in zeros dict besides zero key: ", zeros[key])
+    # print("dict to count nums: ", zeros)
+    # return trailing_zeros_counts
+    if num < 5:
+        return 0
+    return num // 5 + trailing_zeros(num // 5)
 
 
-print(trailing_zeros(6))
+print(trailing_zeros(10))
+print(trailing_zeros(5))
+print(trailing_zeros(11))
+print(trailing_zeros(20))
+
