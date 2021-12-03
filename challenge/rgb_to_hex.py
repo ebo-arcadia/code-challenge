a8b2273389ff0f9d7434b84ceded620f7fa2614e
Ebo Arcadia <yiboli.coding@gmail.com>
2021-12-02 18:24:58 -0800

    add solutions to rgb convertor

--- challenge/rgb_to_hex.py
+++ challenge/rgb_to_hex.py
@@ -30,49 +30,52 @@ DECIMAL = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 HEXADECIMAL = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
 
 
-def rgb_to_hex_convertor(num1, num2, num3):
-    hex_color_code = ""
-    if num1 < 0 or num2 < 0 or num3 < 0:
-        return "pass"
-        # todo: round these nums to the closest valid value
-    else:
-        hex_num = int(num1) / 16
-        integer_part = int(hex_num // 1)
-        decimal_part = int((hex_num % 1) * 16)
-        print("integer part: ", integer_part)
-        print("decimal part: ", decimal_part)
-
-        # todo: find built-in function to convert base-10 int to base-16 int
-        for i in range(0, len(DECIMAL)):
-            for j in range(0, len(HEXADECIMAL)):
-                if DECIMAL[i] == HEXADECIMAL[j]:
-                    print(DECIMAL[j])
-                    return DECIMAL[j]
-                elif integer_part == 10:
-                    return "A"
-                elif integer_part == 11:
-                    return "B"
-                elif integer_part == 12:
-                    return "C"
-                elif integer_part == 13:
-                    return "D"
-                elif integer_part == 14:
-                    return "E"
-                elif integer_part == 15:
-                    return "F"
-
-        # extra bounds: using int function to convert base-16 to base-10
-        base16_to_base10 = int('DC', 16)
-        print("converted from base 16 to base 10? ", base16_to_base10)
-
-        hex_color_code += str(integer_part)
-        hex_color_code += str(decimal_part)
-        print("final hex color code: ", hex_color_code)
-
-        # todo: apply the same code to num2 and num3
-
-        return hex_num
+def base10_to_base16(color_num):
+    hex_decimal_dict = '0123456789ABCDEF'
+    integer_part = color_num // 16
+    decimal_part = color_num % 16
+    print("integer part: ", integer_part)
+    print("decimal part: ", decimal_part)
+    # check if color num is within valid range
+    # if not, around it to be valid
+    if integer_part > 15:
+        integer_part = 15
+        decimal_part = 15
+    elif integer_part < 0:
+        integer_part = 0
+        decimal_part = 0
+    return str(hex_decimal_dict[integer_part]) + str(hex_decimal_dict[decimal_part])
 
 
+def rgb_to_hex_convertor(r, g, b):
+    r = base10_to_base16(r)
+    g = base10_to_base16(g)
+    b = base10_to_base16(b)
+    return r + g + b
+
+
+def rgb2hex(r, g, b):
+
+    def validate_rbg(n):
+        if n < 0: return 0
+        if n > 255: return 255
+        return n
+
+    # check if rgb value is within valid bound
+    r = validate_rbg(r)
+    g = validate_rbg(g)
+    b = validate_rbg(b)
+
+    # convert to hex values
+    to_hex = "%02x%02x%02x" % (r, g, b)
+    return to_hex.upper()
+
+
+print("testing rgb2hex: ", rgb2hex(220, 10, 20))
+print("testing rgb2hex: ", rgb2hex(220, -1, 20))
+print("testing rgb2hex: ", rgb2hex(0, 0, 0))
 print(rgb_to_hex_convertor(220, 10, 20))
-print("values of params out of range: ", rgb_to_hex_convertor(220, -1, 20))
+print("testing convertor with helper function: ", rgb_to_hex_convertor(220, -1, 20))
+print("testing convertor with helper function: ", rgb_to_hex_convertor(220, 10, 20))
+print("testing convertor with helper function: ", rgb_to_hex_convertor(-10, 300, -1))
+print("testing convertor with helper function: ", rgb_to_hex_convertor(0, 0, 0))
