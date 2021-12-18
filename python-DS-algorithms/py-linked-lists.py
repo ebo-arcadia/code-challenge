305cf8083a438132f4a87153455726ef4a9d0b4c
Ebo Arcadia <yiboli.coding@gmail.com>
2021-12-18 12:09:19 -0800

    add linked lists example code create a linked list insert node at the start of a linked list

--- python-DS-algorithms/py-linked-lists.py
+++ python-DS-algorithms/py-linked-lists.py
@@ -13,6 +13,7 @@ class Node:
         self.nodeval = nodeval
         self.nextval = None
 
+
 class LinkedList:
     def __init__(self):
         self.headval = None
@@ -25,6 +26,7 @@ class LinkedList:
             stored_nodes.append(thisnode.nodeval)
             thisnode = thisnode.nextval
         return stored_nodes
+
 
 list1 = LinkedList()
 
@@ -49,4 +51,83 @@ node4.nextval = node5
 # print the value of the next element by assigning the pointer of the next element to the current data element
 print("--------------------")
 
-print("traverse the singly linked list: ", list1.listnodes())
+
+# how to insert a new node at the beginning of a list?
+
+# algorithms
+# 1. define a class represents a node in the list. it has two properties, one value and the other pointing to the next node
+# 2. define a class for creating linked list, with two nodes, head and tail, two methods addNodesAtBeginning() and display()
+# 3. addNodesAtBeginning() will add node to the beginning of the linked list; display() will show all nodes in the list
+
+class Nodes:
+    # represent node of list
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedLists:
+    # declare head and tail pointer
+    def __init__(self):
+        self.head = Nodes(None)
+        self.tail = Nodes(None)
+        self.head.next = self.tail
+        self.tail.next = self.head
+
+    def addNodesAtBeginning(self, value):
+        # create new node to be added
+        newNode = Nodes(value)
+        # check if the list is empty
+        # if empty, point both the head and tail to the new node
+        # the new node is also pointed to the head
+        if self.head.value is None:
+            self.head = newNode
+            self.tail = newNode
+            newNode.next = self.head
+        else:
+            # if the list is not empty
+            # store current first node in a temp var
+            # new node points to the temp as the temp becomes the second / next node
+            # new node becomes the new head
+            # tail also points to the new node
+            temp = self.head
+            newNode.next = temp
+            self.head = newNode
+            self.tail.next = self.head
+
+    def display(self):
+        # define a current that will point to the head
+        # print current.value till current points to head again
+        # current points to the next node in the list in each iteration
+        current_node = self.head
+        current_list = []
+        if self.head is None:
+            print("linked list is empty")
+            return "list is empty"
+        else:
+            # print each node value by incrementing the pointer
+            print("Adding notes to the start of the list: ")
+            print(current_node.value)
+            while current_node.next != self.head:
+                current_node = current_node.next
+                print(current_node.value)
+                current_list.append(current_node.value)
+            print("\n")
+        return current_list
+
+# how to insert a new node at the end of a list?
+# how to insert a new node between two nodes in a list?
+# how to remove a node from a list?
+
+if __name__ == "__main__":
+    print("traverse the singly linked list1: ", list1.listnodes())
+    print("--------------------------------")
+    list1 = LinkedLists()
+    list1.addNodesAtBeginning(1)
+    list1.display()  # display list after adding 1
+    list1.addNodesAtBeginning(10)
+    list1.display()  # display list after adding 10
+    list1.addNodesAtBeginning(999)
+    list1.display()  # display list after adding 999
+    list1.addNodesAtBeginning(8971)
+    print("most current linked list: ", list1.display())
