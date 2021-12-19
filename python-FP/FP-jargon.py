dc695605f531b85b88cc07b0197b7a4caa7f8bd7
Ebo Arcadia <yiboli.coding@gmail.com>
2021-12-18 17:41:19 -0800

    add fountional programming concepts, continuation, currying

--- python-FP/FP-jargon.py
+++ python-FP/FP-jargon.py
@@ -1,5 +1,6 @@
 from inspect import signature
 
+
 # Arity
 # The number of arguments a function takes
 
@@ -7,6 +8,7 @@ def arity_two_concat(x: str, y: str) -> str:
     two_strs = ""
     two_strs += x + y
     return two_strs
+
 
 num_of_args = len(signature(arity_two_concat).parameters)
 print("what is the arity of function arity_two_concat?: ", num_of_args)
@@ -21,7 +23,6 @@ is_a_func_as_args = lambda type_right: lambda x: type(x) is type_right
 if_int = filter_higher_func(is_a_func_as_args(int), [0, "0", 2, "2", "None", None])
 print("filter an iterable and only returns type integer: ", if_int)
 print("-----------------------")
-
 
 # closure scope which captures the local variables of a function for access even after the execution of the function
 # thus moved out of the block in which those variable are defined
@@ -39,4 +40,88 @@ print("-----------------------")
 # see another file in this directory for more detail
 
 # currying
+# a process of converting a function that takes multiple arguments into a founction that takes those
+# arguments one at a time
+
+multiply = lambda x, y: x * y
+curried_multiply = lambda x: lambda y: x * y
+rt = curried_multiply(11)(3)
+print(rt)
+three_times_what = curried_multiply(3)
+three_times_nine_nine = three_times_what(99)
+print(three_times_nine_nine)
+
+print("-----------------------")
+
+# auto-currying
+# transforms a function takes multiple arguments into one
+# if less that correct numbers of arguments is given
+# that function will returns another function to take care of the missing arguments
+# the function will evaluate when it gets the correct amount of arguments
+
+from toolz import curry
+
+
+@curry
+def divide_two_num(a, b):
+    return a / b
+
+
+divide_two = divide_two_num(10, 2)  # returns 5
+print("given the correct amount of args, function returns: ", divide_two)
+one_arg_exec = divide_two_num(100)  # returns (b) = 100 / b
+print("given only one args, function returns: ", one_arg_exec)
+curried_divide = divide_two_num(100)(50)  # returns 2
+print("given two args but in sequence, function returns: ", curried_divide)
+
+print("-----------------------")
+
+# function composition the act of putting two or more functions together to form a new function in such a way that
+# the output of one function is passed as an input for another function
+
+def add_two(x):
+    return x + 2
+
+def times_ten(y):
+    return y * 10
+
+# print the result of composition to first add 2 to a num and then times it with 10
+def add_times_comp(n):
+    return times_ten(add_two(n))
+
+func_comp = add_times_comp(10)
+print("expected result to be 120: ", func_comp)
+print("-----------------------")
+
+# Continuation / Continuation Passive Style (CPS)
+# concept of a given time in a program, the part of the code that has yet to be executed
+# it is often used in asynchronous programming when the program needs to wait to receive data before it continue
+# in other words, at any point of time in the program, one can save the state of the execution for later
+
+continue_program_with = lambda data: data
+
+def find_data(n):
+    if n <= 2:
+        return "done"
+    else:
+        return continue_program_with
+
+
+print(find_data(5))
+
+print("-----------------------")
+
+# purity
+# see python-FP folder for more detail
+
+# side effects
+# see python-FP folder for more details
+
+
+# idempotent
+
+
+
+
+
 
