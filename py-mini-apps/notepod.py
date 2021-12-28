0e58615c8104c3a4e17a7b60a26926158dd4de4b
Ebo Arcadia <yiboli.coding@gmail.com>
2021-12-27 21:07:56 -0500

    add python hash-table

---
+++ py-mini-apps/notepod.py
@@ -0,0 +1,13 @@
+# what is the problem? design and make a simple notepad
+# what features does it have?
+# - GUI menu such as file and edit
+# - functions such as save, add, edit, delete, copy and paste files
+
+import tkinter
+from tkinter import *
+import os
+
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
+
