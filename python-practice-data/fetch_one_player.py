ee3df0a62e982607e941f88dbbdf6e07db6ef792
Ebo Arcadia <yiboli.coding@gmail.com>
2022-04-04 09:21:47 -0400

    add code to query, process, and return data from db using sql

---
+++ python-practice-data/fetch_one_player.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='lahman.csw1rmup8ri6.us-east-1.rds.amazonaws.com',
+    user='python',
+    passwd='python',
+    db='lahmansbaseballdb'
+)
+
+query = """
+        SELECT nameFirst, nameLast, birthCity, birthState, birthYear
+        FROM people
+        WHERE playerID = 'jeterde01';
+        """
+
+cursor = connection.cursor(dictionary=True)
+cursor.execute(query)
+result = cursor.fetchone()
+
+if result:
+    player_name = result['nameFirst'] + ' ' + result['nameLast']
+    birth_place = result['birthCity'] + ', ' + result['birthState']
+    birth_year = result['birthYear']
+    print(f'{player_name} was born in {birth_place} in {birth_year}')
+else:
+    print("no player was found")
+
+cursor.close()
+connection.close()
+
+
+
