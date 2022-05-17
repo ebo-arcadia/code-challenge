502dbed466813a8b36a251ea6fbd3a3423905bfb
Ebo Arcadia <yiboli.coding@gmail.com>
2022-05-17 09:57:58 -0400

    use alias to reference table name for operation; use update() to update column in table

--- python-SQLAlchemy/basics.py
+++ python-SQLAlchemy/basics.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, text
-from sqlalchemy.sql import select, subquery
+from sqlalchemy.sql import select, alias
+from sqlalchemy.sql.expression import update
 
 # create db engine object
 # create metadata to stores db info
 
 engine = create_engine('sqlite:///college.db', echo=True)
 meta = MetaData()
-meta.create_all(engine)
+meta.bind = engine
 
 students = Table(
     'students', meta,
@@ -14,6 +15,8 @@ students = Table(
     Column('name', String),
     Column('lastname', String),
 )
+
+meta.create_all(engine)
 
 # insert columns in db
 students_to_inserts = students.insert().values(name='Ravi', lastname='Kapoor')
@@ -44,6 +47,34 @@ text_query_result = db_connection.execute(sql_query_text)
 
 for row in text_query_result:
     print(row, sep=" ")
+
+
+text_obj = text("select students.name, students.lastname from students where students.name between :x and :y")
+db_connection.execute(text_obj, x='A', y='L').fetchall()
+print("----------", sep=" ")
+print("query database using alias")
+st = students.alias("s")
+s = select([st]).where(st.c.id > 2)
+st_with_alias = db_connection.execute(s).fetchall()
+
+print(st_with_alias)
+
+# update a value in a column
+print("--------------")
+print("update column with update object")
+conn = engine.connect()
+update_stmt = students.update().where(students.c.name == "Ravi").values(name="Leon")
+conn.execute(update_stmt)
+updated_students_obj = students.select()
+update_students = db_connection.execute(updated_students_obj).fetchall()
+print(update_students)
+
+print("--------------")
+print("update column with update() method")
+update_column = update(students).where(students.c.name == "Ravi").values(name="Snake")
+
+
+
 
 
 
