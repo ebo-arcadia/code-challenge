3c1913da76f000c6c7d21d460adbcff36979728d
Ebo Arcadia <yiboli.coding@gmail.com>
2022-06-08 09:33:39 -0400

    fix conflict when pushing code change to remote

--- python-SQLAlchemy/many-to-many.py
+++ python-SQLAlchemy/many-to-many.py
@@ -41,20 +41,14 @@ b3 = Benchmark(name='benchmark bronze')
 s3 = Sleeve(name='sleeve cylinder')
 b3.sleeves.append(s3)
 
-<<<<<<< HEAD
 # above objects are in transient state saved in the memory only
 
-=======
->>>>>>> 28af19b3b970c1d83f7130b05dfb670a32853b31
 # causing integrity error
 # s1.benchmarks.append(b1)
 # s2.benchmarks.append(b2)
 # s3.benchmarks.append(b3)
 
-<<<<<<< HEAD
 # cascading objects into session object
-=======
->>>>>>> 28af19b3b970c1d83f7130b05dfb670a32853b31
 Session = sessionmaker(bind=db_engine)
 db_connector = Session()
 # db_connector.add(b1)
@@ -67,7 +61,6 @@ db_connector.add_all([b1, b2, b3])
 # db_connector.add(s3)
 db_connector.commit()
 
-<<<<<<< HEAD
 # query, loading and display data from the tables
 # get all the rows from the two tables
 for data in db_connector.query(Benchmark, Sleeve). \
@@ -83,6 +76,3 @@ for data in db_connector.query(Benchmark, Sleeve). \
 # each database requires a DBAPI wrapper; each wrapper requries a driver
 # SQL alchemy uses create_engine class to construct and passing parameters including username, DB names, etc
 
-=======
-# query and display data from the tables
->>>>>>> 28af19b3b970c1d83f7130b05dfb670a32853b31
